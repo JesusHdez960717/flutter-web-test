a58af7b9aa09270f8eff56cf630d29e74fa23e52
Jesus Hernandez <jhernandezb96@gmail.com>
2023-09-15 13:54:02 -0400

    Fix min name

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Root101',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
